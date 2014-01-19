b705a9195ce25200d5d36bafd5fd7513865bed46
Todd Gerspacher <toddgerspacher27@gmail.com>
2014-01-19 16:20:16 -0500

    restructured & formatted app11a

--- Chapter-11-The-Persistence-Layer/src/app11a/dao/DAO.java
+++ Chapter-11-The-Persistence-Layer/src/app11a/dao/DAO.java
@@ -3,5 +3,5 @@ package app11a.dao;
 import java.sql.Connection;
 
 public interface DAO {
-	public Connection getConnection() throws DAOException;  
+   public Connection getConnection() throws DAOException;
 }
