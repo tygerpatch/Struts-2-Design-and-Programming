294898fb4ebc5d596492840a5d4885e8a7629ec2
Todd Gerspacher <toddgerspacher27@gmail.com>
2014-01-19 20:27:45 -0500

    made the name of the database a context-param variable in web.xml

--- Chapter-11-The-Persistence-Layer/src/app11a/dao/DAOBase.java
+++ Chapter-11-The-Persistence-Layer/src/app11a/dao/DAOBase.java
@@ -2,20 +2,23 @@ package app11a.dao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+
 import javax.servlet.ServletContext;
 import javax.sql.DataSource;
+
 import org.apache.struts2.ServletActionContext;
 
 public class DAOBase implements DAO {
    public Connection getConnection() throws DAOException {
       ServletContext servletContext = ServletActionContext.getServletContext();
       DataSource dataSource = (DataSource) servletContext.getAttribute("dataSource");
+      // The dataSource attribute is set in AppListener's contextInitialized method
       Connection connection = null;
       if (dataSource != null) {
          try {
             connection = dataSource.getConnection();
-            // TODO: make database name a context-param in web.xml
-            connection.setCatalog("my_jdbc");
+            String databaseName = servletContext.getInitParameter("databaseName");
+            connection.setCatalog(databaseName);
          }
          catch (SQLException e) {
             System.out.println("DAOBase");
