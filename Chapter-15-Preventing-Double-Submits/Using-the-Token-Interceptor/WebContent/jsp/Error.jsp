1c0629c2479b7c1a0cba83b2c8a8c5b7324bf589
Todd Gerspacher <toddgerspacher27@gmail.com>
2014-02-04 19:38:18 -0500

    got app15a to work

---
+++ Chapter-15-Preventing-Double-Submits/Using-the-Token-Interceptor/WebContent/jsp/Error.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<html>
+   <head>
+      <title>Thank you</title>
+      <style type="text/css">
+         @import url(css/main.css);
+      </style>
+   </head>
+   <body>
+      <div id="global">
+         <s:actionerror />
+      </div>
+      <hr />
+      <h3>Debug Tag</h3>
+      <s:debug />
+   </body>
+</html>
---
+++ Chapter-15-Preventing-Double-Submits/Using-the-Token-Interceptor/src/app15a/Payment.java
@@ -0,0 +1,76 @@
+package app15a;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Payment extends ActionSupport {
+   public String execute() {
+      System.out.println("------------------------EXECUTE");
+      // simulate a long processing task
+      try {
+         Thread.sleep(6000);
+      }
+      catch (InterruptedException e) {
+         System.out.println("InterruptedException !!!");
+         e.printStackTrace();
+      }
+      return SUCCESS;
+   }
+
+   // *** amount property
+   private double amount;
+
+   public double getAmount() {
+      return amount;
+   }
+
+   public void setAmount(double amount) {
+      this.amount = amount;
+   }
+
+   // *** creditCardType property
+   private int creditCardType;
+
+   public int getCreditCardType() {
+      return creditCardType;
+   }
+
+   public void setCreditCardType(int creditCardType) {
+      this.creditCardType = creditCardType;
+   }
+
+   // *** nameOnCard property
+   private String nameOnCard;
+
+   public String getNameOnCard() {
+      return nameOnCard;
+   }
+
+   public void setNameOnCard(String nameOnCard) {
+      this.nameOnCard = nameOnCard;
+   }
+
+   // *** number property
+   private String number;
+
+   public String getNumber() {
+      return number;
+   }
+
+   public void setNumber(String number) {
+      this.number = number;
+   }
+
+   // *** expiryDate property
+   private String expiryDate;
+
+   public String getExpiryDate() {
+      return expiryDate;
+   }
+
+   public void setExpiryDate(String expiryDate) {
+      this.expiryDate = expiryDate;
+   }
+}
